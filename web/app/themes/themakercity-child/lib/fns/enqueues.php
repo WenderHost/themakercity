2cd7ac72468d0ad8d3ff924c39d1180ac4bed47c
Michael Wender <michael@michaelwender.com>
2024-01-30 11:25:28 -0500

    Updating location of distributon css

--- web/app/themes/themakercity-child/lib/fns/enqueues.php
+++ web/app/themes/themakercity-child/lib/fns/enqueues.php
@@ -7,8 +7,8 @@ namespace TheMakerCity\enqueues;
  * @return void
  */
 function enqueue_scripts_styles() {
-  $main_css = ( 'development' != MAKR_ENV )? 'dist/main.css' : 'main.css' ;
-  wp_enqueue_style( 'bizplanner', get_stylesheet_directory_uri() . '/lib/css/' . $main_css, null, filemtime( get_stylesheet_directory() . '/lib/css/' . $main_css ) );
+  $css_dir = ( 'development' != MAKR_ENV )? 'css-dist' : 'css' ;
+  wp_enqueue_style( 'bizplanner', get_stylesheet_directory_uri() . '/lib/' . $css_dir . '/main.css', null, filemtime( get_stylesheet_directory() . '/lib/' . $css_dir . '/main.css' ) );
   wp_enqueue_script( 'font-awesome', 'https://kit.fontawesome.com/f4de4ed03f.js', null, '1.0.0', false );
 }
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\enqueue_scripts_styles', 20 );
