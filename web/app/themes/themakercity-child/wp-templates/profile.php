a1570e111d709b544eae7ae0e3f53d9afe82a37d
Michael Wender <michael@michaelwender.com>
2024-04-01 18:32:17 -0400

    Adding link to view profile, Adding Post Title field

--- web/app/themes/themakercity-child/wp-templates/profile.php
+++ web/app/themes/themakercity-child/wp-templates/profile.php
@@ -2,15 +2,17 @@
 <?php
 $current_user = wp_get_current_user();
 if( $current_user ){
+  $maker_profile_id = get_user_meta( $current_user->ID, 'maker_profile_id', true );
+
   $settings = [
-    'updated_message'       => __('Your profile has been updated.', 'acf'),
+    'post_title'            => true,
+    'updated_message'       => __('Your profile has been updated. <a href="' . get_permalink( $maker_profile_id ) . '" target="_blank">View</a> your profile.', 'acf'),
     'html_updated_message'  => '<div class="alert alert-success" role="alert"><div class="alert-message">%s</div></div>',
     'html_submit_button'    => '<button type="submit" class="btn btn-primary">%s</button>',
     'instruction_placement' => 'field',
     'fields'  => [ 'name', 'email', 'collaborator', 'maker_category', 'primary_image', 'additional_images', 'description', 'social_profiles', 'avatar'  ],
   ];
 
-  $maker_profile_id = get_user_meta( $current_user->ID, 'maker_profile_id', true );
   if( empty( $maker_profile_id ) ){
     $settings['post_id'] = 'new_post';
     $settings['new_post'] = [
@@ -25,3 +27,18 @@ if( $current_user ){
   acf_form( $settings );
 }
 ?>
+<style>
+  /* This CSS file is to hide the original Title label */
+  .acf-field .acf-label label[for="acf-_post_title"] {
+    display: none; /* Hide the original label */
+  }
+</style>
+<script type="text/javascript">
+jQuery(document).ready(function($) {
+  // Change the Title field placeholder to "Business Name"
+  $('input[name="acf[_post_title]"]').attr('placeholder', 'Business Name');
+
+  // Optionally, add a new label element if you want it to be visible and not just a placeholder
+  $('input[name="acf[_post_title]"]').before('<div class="acf-label"><label for="acf[_post_title]">Business Name <span class="acf-required">*</span></label></div>');
+});
+</script>
