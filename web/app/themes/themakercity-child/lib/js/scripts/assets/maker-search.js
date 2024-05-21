061ea21f8ff77d9687479c400d6929f136102ecb
Michael Wender <michael@michaelwender.com>
2024-05-21 11:01:08 -0400

    Refining Maker Directory Search initialization

--- web/app/themes/themakercity-child/lib/js/scripts/assets/maker-search.js
+++ web/app/themes/themakercity-child/lib/js/scripts/assets/maker-search.js
@@ -18,16 +18,28 @@ function activateSecondTab() {
 function setupTabs() {
   const tabTitles = document.querySelectorAll('.elementor-tab-title');
   const filterByTab = document.getElementById('elementor-tab-title-1631'); // Assuming this is the "Filter by:" tab
+  filterByTab.textContent = 'Initializing...';
+  filterByTab.classList.add('button');
   const categoryTab = document.getElementById('elementor-tab-title-1632');
 
   tabTitles.forEach(tab => {
     tab.addEventListener('click', () => {
+      /**
+       * If we click on a tab that IS NOT the first tab, this means
+       * we have "Opened" the filters. So we set the text of the
+       * first tab to be "Close Filter Menu":
+       */
       if (tab.id !== 'elementor-tab-title-1631') {
-        if (filterByTab.textContent === 'Filter by:') {
+        if (filterByTab.textContent === 'Initializing...' || filterByTab.textContent === 'Filter by:') {
           filterByTab.textContent = 'Close Filter Menu';
           filterByTab.classList.add('button');
         }
       } else {
+        /**
+         * When we click directly on the first tab, we have "Closed"
+         * all the filters. So, we set the text of this tab back to
+         * "Filter by:":
+         */
         filterByTab.textContent = 'Filter by:';
         filterByTab.classList.remove('button');
       }
@@ -36,8 +48,15 @@ function setupTabs() {
 }
 
 export function initializeMakerSearch() {
-  document.addEventListener('DOMContentLoaded', function() {
-    runWhenElementorLoaded(activateSecondTab);
-    setupTabs();
+  let inite = 0;
+  document.addEventListener('DOMContentLoaded', async function() {
+    await setupTabs();
+    //runWhenElementorLoaded(activateSecondTab);
+    setInterval(function(){
+      if (document.readyState == "complete" && inite == 0 )  {
+        activateSecondTab();
+        inite++;
+      }
+    }, 500);
   });
 }
