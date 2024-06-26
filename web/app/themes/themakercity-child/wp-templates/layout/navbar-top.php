87858e0795b008394ed4d8eb621ae640fea0fdc2
Michael Wender <michael@michaelwender.com>
2024-06-26 16:55:13 -0400

    Setting z-index of top bar nav dropdown

--- web/app/themes/themakercity-child/wp-templates/layout/navbar-top.php
+++ web/app/themes/themakercity-child/wp-templates/layout/navbar-top.php
@@ -160,7 +160,7 @@ $avatar = get_avatar_url( $current_user->user_email, ['size' => 96] );
               <a class="nav-link dropdown-toggle d-none d-sm-inline-block" href="#" data-bs-toggle="dropdown">
                 <img src="<?= $avatar ?>" class="avatar img-fluid rounded me-1" alt="<?= $current_user->user_firstname ?> <?= $current_user->user_lastname ?>" /> <span class="text-dark"><?= $current_user->user_firstname ?> <?= $current_user->user_lastname ?></span>
               </a>
-              <div class="dropdown-menu dropdown-menu-end">
+              <div class="dropdown-menu dropdown-menu-end" style="z-index: 9999;">
                 <a class="dropdown-item" href="<?= home_url('/profile') ?>"><i class="align-middle me-1" data-feather="user"></i> Profile</a>
                 <?php if( current_user_can( 'activate_plugins' ) ){ ?>
                 <a class="dropdown-item" href="<?= site_url( '/wp-admin/' ) ?>" target="_blank"><i class="fas fa-arrow-up-right-from-square"></i> WP Admin</a>
