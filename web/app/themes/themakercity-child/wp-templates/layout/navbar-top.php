f18677264ab04cedd179a1edabc842b2cb365718
Michael Wender <michael@michaelwender.com>
2025-01-20 22:33:37 -0500

    Removing fullscreen button

--- web/app/themes/themakercity-child/wp-templates/layout/navbar-top.php
+++ web/app/themes/themakercity-child/wp-templates/layout/navbar-top.php
@@ -9,13 +9,6 @@ $avatar = get_avatar_url( $current_user->user_email, ['size' => 96] );
 
         <div class="navbar-collapse collapse">
           <ul class="navbar-nav navbar-align">
-            <li class="nav-item">
-              <a class="nav-icon js-fullscreen d-none d-lg-block" href="#">
-                <div class="position-relative">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-maximize align-middle"><path d="M8 3H5a2 2 0 0 0-2 2v3m18 0V5a2 2 0 0 0-2-2h-3m0 18h3a2 2 0 0 0 2-2v-3M3 16v3a2 2 0 0 0 2 2h3"></path></svg>
-                </div>
-              </a>
-            </li>
             <li class="nav-item dropdown">
               <a class="nav-icon dropdown-toggle d-inline-block d-sm-none" href="#" data-bs-toggle="dropdown">
                 <i class="align-middle" data-feather="settings"></i>
