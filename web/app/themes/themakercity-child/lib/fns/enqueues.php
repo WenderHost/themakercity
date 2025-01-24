6734c18791094ff5d9f811996dabcaeecf6d27d2
Michael Wender <michael@michaelwender.com>
2025-01-24 16:30:39 -0500

    Re-adding AdminKit app.js after recompiling w/out jQuery

--- web/app/themes/themakercity-child/lib/fns/enqueues.php
+++ web/app/themes/themakercity-child/lib/fns/enqueues.php
@@ -10,6 +10,9 @@ function enqueue_scripts_styles() {
   $css_dir = ( 'development' != MAKR_ENV )? 'css-dist' : 'css' ;
   wp_enqueue_style( 'makercity', MAKR_STYLESHEET_DIR_URI . 'lib/' . $css_dir . '/main.css', null, filemtime( MAKR_STYLESHEET_DIR . 'lib/' . $css_dir . '/main.css' ) );
   wp_enqueue_script( 'font-awesome', 'https://kit.fontawesome.com/f4de4ed03f.js', null, '1.0.0', false );
+
+  // AdminKit Scripts/Styles
+  wp_register_script( 'adminkit', MAKR_STYLESHEET_DIR_URI . 'adminkit-pro/dist/js/app.js', null, filemtime( MAKR_STYLESHEET_DIR . 'adminkit-pro/dist/js/app.js' ), true );
   wp_register_style( 'adminkit', MAKR_STYLESHEET_DIR_URI . 'adminkit-pro/dist/css/light.css', null, filemtime( MAKR_STYLESHEET_DIR . 'adminkit-pro/dist/css/light.css' ) );
 
   wp_register_style( 'choicesjs', MAKR_STYLESHEET_DIR_URI . 'lib/css-dist/choices.min.css', null, filemtime( MAKR_STYLESHEET_DIR . 'lib/css-dist/choices.min.css' ) );
@@ -20,6 +23,7 @@ function enqueue_scripts_styles() {
   wp_register_style( 'filepond', MAKR_STYLESHEET_DIR_URI . 'lib/js/dist/filepond.init.css', null, filemtime( MAKR_STYLESHEET_DIR . 'lib/js/dist/filepond.init.css' ) );
   //wp_register_script( 'filepond', 'https://unpkg.com/filepond@^4/dist/filepond.js' );
   wp_register_script( 'filepond-init', MAKR_STYLESHEET_DIR_URI . 'lib/js/dist/filepond.init.js', null, filemtime( MAKR_STYLESHEET_DIR . 'lib/js/dist/filepond.init.js' ), true );
+
 }
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\enqueue_scripts_styles', 20 );
 
