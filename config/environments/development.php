0c89402cf679b263bfde3a722ec88182d6eff23d
Michael Wender <michael@michaelwender.com>
2025-02-20 19:02:54 -0500

    Disabling SMTP2GO in Development

--- config/environments/development.php
+++ config/environments/development.php
@@ -22,4 +22,5 @@ Config::define('DISALLOW_FILE_MODS', false);
 Config::define('DISABLED_PLUGINS', [
     'spinupwp/spinupwp.php',
     'limit-login-attempts-reloaded/limit-login-attempts-reloaded.php',
+    'smtp2go/smtp2go-wordpress-plugin.php',
 ]);
